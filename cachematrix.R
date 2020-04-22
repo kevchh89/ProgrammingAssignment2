3f8ddd5840c1b9141b8d2fe71ebb9c5b09214bf1
kevchh89 <47668597+kevchh89@users.noreply.github.com>
2020-04-21 20:39:51 -0400

    Assignament finish

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  x<-matrix(sample(1:25,replace = T),5,5)
-  print(x)
+makeCacheMatrix<- function(x = matrix()) {
+  m <- NULL
+  mtx2<-matrix(x,sqrt(length(x)),sqrt(length(x)))
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() mtx2
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-## The function creates a 5x5 matrix with a sample of random integer numbers
+## The function imputes a matrix from a single vector or a matrix imputed
 
 cacheSolve <- function(x, ...) {
-  solve(x)
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
